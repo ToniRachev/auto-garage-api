f3e4462ba28b2c7d07a0e1c8a42aa18a12c0deca
Toni <tonirachev92@gmail.com>
2026-02-21 12:45:13 +0200

    feat: Add UserResource for transforming user data into JSON format

---
+++ app/Http/Resources/V1/UserResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Models\User;
+
+/**
+ * @mixin User
+ */
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'createdAt' => $this->created_at,
+            'updatedAt' => $this->updated_at
+        ];
+    }
+}
