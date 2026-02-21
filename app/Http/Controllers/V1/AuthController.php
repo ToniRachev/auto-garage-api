710f390f806fe510b96eca9bb88ed71b6e9b1502
Toni <tonirachev92@gmail.com>
2026-02-21 13:17:44 +0200

    feat: extract registration logic to auth service

--- app/Http/Controllers/V1/AuthController.php
+++ app/Http/Controllers/V1/AuthController.php
@@ -6,11 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\LoginAuthRequest;
 use App\Http\Requests\V1\RegisterAuthRequest;
 use App\Http\Resources\V1\UserResource;
-use App\Models\User;
 use App\Responses\V1\ApiResponse;
 use App\Services\V1\AuthService;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
@@ -33,20 +30,7 @@ class AuthController extends Controller
 
     public function register(RegisterAuthRequest $request)
     {
-        // Wrap in a transaction to ensure user and token are created together or not at all
-        $result = DB::transaction(function () use ($request) {
-            $user = User::create([
-                "name" => $request->validated('name'),
-                'email' => $request->validated('email'),
-                'password' => Hash::make($request->validated('password')),
-            ]);
-
-            $token = $user->createToken("$user->name auth token")->plainTextToken;
-
-            return compact('user', 'token');
-        });
-
-
+        $result = $this->authService->register($request->validated());
 
         return ApiResponse::created(
             'User was created successfully',
--- app/Services/V1/AuthService.php
+++ app/Services/V1/AuthService.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Laravel\Sanctum\PersonalAccessToken;
 use App\Exceptions\InvalidCredentialsException;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 final class AuthService
 {
@@ -21,6 +22,22 @@ final class AuthService
         $token = $this->generateToken($user);
 
         return compact('user', 'token');
+    }
+
+    public function register($data): array
+    {
+        // Wrap in a transaction to ensure user and token are created together or not at all
+        return DB::transaction(function () use ($data) {
+            $user = User::create([
+                "name" => $data['name'],
+                'email' => $data['email'],
+                'password' => Hash::make($data['password']),
+            ]);
+
+            $token = $user->createToken("$user->name auth token")->plainTextToken;
+
+            return compact('user', 'token');
+        });
     }
 
     private function generateToken(User $user): string
