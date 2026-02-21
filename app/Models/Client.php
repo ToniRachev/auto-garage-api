c71c0b9b58ff2623581382cfb7ffc707af7e0851
Toni <tonirachev92@gmail.com>
2026-02-21 14:12:30 +0200

    feat: enhance Client model documentation with property annotations

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,7 +2,19 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property string $name
+ * @property string $email
+ * @property string $phone
+ * @property string $address
+ * @property string $city
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * 
+ */
 
 class Client extends Model
 {
