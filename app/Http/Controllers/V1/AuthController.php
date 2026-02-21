9ec23dca7d3a7a4f14c43a0f55f7a30dd6ef4519
Toni <tonirachev92@gmail.com>
2026-02-21 12:45:05 +0200

    feat: Implement user registration in AuthController with token generation

--- app/Http/Controllers/V1/AuthController.php
+++ app/Http/Controllers/V1/AuthController.php
@@ -3,11 +3,27 @@
 namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\RegisterAuthRequest;
+use App\Http\Resources\V1\UserResource;
+use App\Models\User;
+use App\Responses\V1\ApiResponse;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     public function login(Request $request) {}
 
-    public function register(Request $request) {}
+    public function register(RegisterAuthRequest $request)
+    {
+        $user = User::create($request->validated());
+        $token = $user->createToken("$user->name auth token")->plainTextToken;
+
+        return ApiResponse::created(
+            'User was created successfully',
+            [
+                'user' => $user,
+                'token' => $token
+            ]
+        );
+    }
 }
