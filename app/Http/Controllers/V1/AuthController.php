05d79237d650fbfbd8f723660bd0a0913d9fba58
Toni <tonirachev92@gmail.com>
2026-02-21 12:07:32 +0200

    feat: Add AuthController with login and register methods and define auth routes

---
+++ app/Http/Controllers/V1/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request) {}
+
+    public function register(Request $request) {}
+}
