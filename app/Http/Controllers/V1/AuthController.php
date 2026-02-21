d4155aab27ffbf33837ddeb7f485f6ba5d7d3d97
Toni <tonirachev92@gmail.com>
2026-02-21 13:13:54 +0200

    feat: Refactor AuthController to implement login method with AuthService integration

--- app/Http/Controllers/V1/AuthController.php
+++ app/Http/Controllers/V1/AuthController.php
@@ -3,17 +3,33 @@
 namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\LoginAuthRequest;
 use App\Http\Requests\V1\RegisterAuthRequest;
 use App\Http\Resources\V1\UserResource;
 use App\Models\User;
 use App\Responses\V1\ApiResponse;
-use Illuminate\Http\Request;
+use App\Services\V1\AuthService;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
-    public function login(Request $request) {}
+    public function __construct(
+        private AuthService $authService
+    ) {}
+
+    public function login(LoginAuthRequest $request)
+    {
+        $result = $this->authService->login($request->validated());
+
+        return ApiResponse::ok(
+            'User loggin successfully.',
+            [
+                'user' => new UserResource($result['user']),
+                'token' => $result['token'],
+            ]
+        );
+    }
 
     public function register(RegisterAuthRequest $request)
     {
